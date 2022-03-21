79cc5dc703a95d3235b56338bfb5fd501a9041e2
Hector <hmgarcia06@gmail.com>
2022-03-21 04:59:03 -0400

    Added a lib

---
+++ chapter_7_packages_crates_modules_paths/restaurant/src/tests/test_one.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod test_one {
+    #[test]
+    fn it_works_test_one() {
+        let result = 2 + 2;
+        println!("it_works_test_one in in test_one.rs");
+        assert_eq!(result, 4);
+        
+    }
+}
