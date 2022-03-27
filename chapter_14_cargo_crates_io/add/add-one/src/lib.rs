fd850e1e263b1291e332a7b5d2e6be5d92834df2
Hector <hmgarcia06@gmail.com>
2022-03-27 08:59:04 -0400

    Chapter 14 cargo & crates done

--- chapter_14_cargo_crates_io/add/add-one/src/lib.rs
+++ chapter_14_cargo_crates_io/add/add-one/src/lib.rs
@@ -1,12 +1,20 @@
+// use rand;
 pub fn add_one(x: i32) -> i32 {
     x + 1
 }
 
 #[cfg(test)]
 mod tests {
+    
+    use super::{add_one};
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+    
+    #[test]
+    fn it_works2() {
+        assert_eq!(3, add_one(2));
     }
 }
--- chapter_14_cargo_crates_io/add/adder/src/main.rs
+++ chapter_14_cargo_crates_io/add/adder/src/main.rs
@@ -1,3 +1,13 @@
+use add_one::{add_one};
+// use rand;
+
 fn main() {
-    println!("Hello, world!");
+    let num = 10;
+    println!(
+        "Hello, world! {} plus one is {}!",
+        num,
+        add_one::add_one(num)
+    );
+    // println!("Hello, world!");
+    println!("Calling add_one fn(add_one(1)) ->  {}", add_one(1));
 }
