475ae4102bf3d635c50353f1e506a67ce502006b
Hector <hmgarcia06@gmail.com>
2022-03-26 23:24:27 -0400

    almost with command line program

--- chapter_12_commandline_project_minigrep/src/main.rs
+++ chapter_12_commandline_project_minigrep/src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::process;
+use minigrep::{Config, run};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world of commandline programs!");
+    let arguments: Vec<String> = env::args().collect();
+    let config = Config::new(&arguments).unwrap_or_else(|error| {
+        println!("Problem parsing arguments: {}", error);
+        process::exit(1);
+    });
+    
+    if let Err(error) = run(config) {
+        println!("Application error: {:?}", error);
+    }
 }
