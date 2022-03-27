7cbdcb0e504666d3804ff03b1b3fd440fd819e52
Hector <hmgarcia06@gmail.com>
2022-03-27 01:14:29 -0400

    Chapter 12 done

--- chapter_12_commandline_project_minigrep/src/main.rs
+++ chapter_12_commandline_project_minigrep/src/main.rs
@@ -6,11 +6,12 @@ fn main() {
     println!("Hello, world of commandline programs!");
     let arguments: Vec<String> = env::args().collect();
     let config = Config::new(&arguments).unwrap_or_else(|error| {
-        println!("Problem parsing arguments: {}", error);
+        eprintln!("Problem parsing arguments: {}", error);
         process::exit(1);
     });
     
     if let Err(error) = run(config) {
-        println!("Application error: {:?}", error);
+        eprintln!("Application error: {:?}", error);
+        // process::exit(1);
     }
 }
