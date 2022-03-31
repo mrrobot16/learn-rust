d0339b36c959ba10b7dc8a1d52b093a21e6cb5a0
Hector <hmgarcia06@gmail.com>
2022-03-31 01:48:43 -0400

    Chapter 15.5 progress

--- chapter_15_smart_pointers/smart-pointers/src/lib.rs
+++ chapter_15_smart_pointers/smart-pointers/src/lib.rs
@@ -1,5 +1,17 @@
 use std::fmt;
-use std::ops::{Deref};
+use std::ops::{Deref, Drop};
+use std::rc::{Rc,};
+
+#[derive(Debug)]
+pub struct CustomSmartPointer {
+    pub data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
 
 #[derive(Debug)]
 pub struct MyBox<T>(T);
@@ -18,16 +30,15 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
-pub fn hello(name: &str) {
-    println!("Hello, {}!", name);
-}
-
 #[derive(Debug)]
 pub enum List {
-    Cons(i32, Box<List>),
+    // Cons(i32, Box<List>),
     // NOTE Below code does not work because Cons second value need to have a size known like 
     // ie: A Box smart pointer.
     // Cons(i32, List),
+    
+    // reference cointing list.
+    Cons(i32, Rc<List>),
     Nil,
 }
 
@@ -36,4 +47,8 @@ impl fmt::Display for List {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
        write!(f, "{:?}", self)
     }
+}
+
+pub fn hello(name: &str) {
+    println!("Hello, {}!", name);
 }
