8401bec8244d950874fa4874a9b2fe1deb7ded3f
Hector <hmgarcia06@gmail.com>
2022-03-31 22:20:43 -0400

    Chapter 16 Fearless concurrency done

--- chapter_16_concurrency/concurrency/src/main.rs
+++ chapter_16_concurrency/concurrency/src/main.rs
@@ -1,11 +1,36 @@
-use std::sync::Mutex;
+use std::sync::{Mutex, Arc};
+use std::thread;
+use std::rc::Rc;
 
 fn main() {
     println!("Hello, world of Concurrency");
-    let mutex = Mutex::new(50);
-    {
-        let mut number = mutex.lock().unwrap();
-        *number = 6;
+    
+    let counter = Rc::new(Mutex::new(0));
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    // 
+    for _ in 0..10 {
+        // this below give errror, for ref counting inside multiple threads is better to use Arc.
+        // let counter = Rc::clone(&counter);
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+    
+            *num += 1;
+        });
+        handles.push(handle);
     }
-    println!("mutex = {:?}", mutex);
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+    
+    // let mutex = Mutex::new(50);
+    // {
+    //     let mut number = mutex.lock().unwrap();
+    //     *number = 6;
+    // }
+    // println!("mutex = {:?}", mutex);
 }
