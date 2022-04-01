9fd20436c75479efa6a3e4dddc36c9b7f669f965
Hector <hmgarcia06@gmail.com>
2022-04-01 19:53:05 -0400

    Gui project finished

--- chapter_17_00P/gui/src/main.rs
+++ chapter_17_00P/gui/src/main.rs
@@ -1,3 +1,36 @@
+use gui::{Button, Screen, SelectBox};
+
+// All this does not work.
+
 fn main() {
     println!("Hello world of OOP GUI");
+    
+    let select_box = Box::new(SelectBox {
+        width: 75,
+        height: 10,
+        options: vec![
+            String::from("Yes"),
+            String::from("Maybe"),
+            String::from("No"),
+        ],
+    });
+    
+    let button = Box::new(Button {
+        width: 50,
+        height: 10,
+        label: String::from("OK"),        
+    });
+
+    let screen = Screen {
+        components: vec![
+            select_box,
+            button,
+        ]
+    };
+    
+    // let screen = Screen {
+    //     components: vec![Box::new(String::from("Hi"))],
+    // };
+
+    screen.run();
 }
