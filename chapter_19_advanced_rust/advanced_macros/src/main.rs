beea2cd608e172366325307d2e2a5221973515f5
Hector <hmgarcia06@gmail.com>
2022-04-02 19:59:42 -0400

    Created hello_macro crate

--- chapter_19_advanced_rust/advanced_macros/src/main.rs
+++ chapter_19_advanced_rust/advanced_macros/src/main.rs
@@ -1,8 +1,7 @@
 // 19.30
-use advanced_macros::{define_variables, Pancakes, HelloMacro};
+use advanced_macros::{Pancakes, HelloMacro};
 
 fn main() {
     println!("Hello, world of Macros!!");
-    // define_variables();
     Pancakes::hello_macro();
 }
